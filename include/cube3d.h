455367cf24a65a6e3682df5bfdddbee544a8a685
Maria Fornovi Gomez <mfornovi@c3r8s2.42madrid.com>
2025-12-03 17:02:40 +0100

    this is (not) going great

--- include/cube3d.h
+++ include/cube3d.h
@@ -1,8 +1,35 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
-#include <math.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cube3d.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfornovi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 14:43:35 by mfornovi          #+#    #+#             */
+/*   Updated: 2025/12/03 14:43:40 by mfornovi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUBE3D_H
+# define CUBE3D_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <string.h>
+# include <math.h>
+
+typedef struct s_compass
+{
+	mlx_image_t	*no;
+	mlx_image_t *so;
+	mlx_image_t *we;
+	mlx_image_t *ea;
+	int		f;
+	int		c;
+}	t_compass;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -25,6 +25,16 @@ void	print_accepted_input(void)
 	printf("		(maps available in the \"maps\" directory)\n");
 }
 
+void	init_compass(t_compass *comp)
+{
+	comp->no->enabled = false;
+	comp->so->enabled = false;
+	comp->we->enabled = false;
+	comp->ea->enabled = false;
+	comp->f = -1;
+	comp->c = -1;
+}
+
 bool	valid_file(char *file_name)
 {
 	int	i;
@@ -37,27 +47,68 @@ bool	valid_file(char *file_name)
 	return (true);
 }
 
-bool	parse_input(int fd)
+bool	set_path_to_struct(mlx_image_t *img, char *buf )
+{
+	int	i;
+
+	i = -1;
+	while (buf[++i] == ' ' || buf[i] == '	');
+	buf = buf + (i - 1);
+
+	img = mlx_load_png(buf);
+}
+
+bool	fill_structure_bit(char *buf, t_compass *comp)
+{
+	int	i;
+
+	if (buf[0] == 'N' && buf[1] == 'O' && (buf[2] == ' ' || buf[2] == '	'))
+		if (!set_path_to_struct(comp->no, buf + 2))
+			return(false);
+	if (buf[0] == 'S' && buf[1] == 'O' && (buf[2] == ' ' || buf[2] == '	'))
+		if (!set_path_to_struct(comp->so, buf + 2))
+			return(false);
+	if (buf[0] == 'W' && buf[1] == 'E' && (buf[2] == ' ' || buf[2] == '	'))
+		if (!set_path_to_struct(comp->we, buf + 2))
+			return(false);
+	if (buf[0] == 'E' && buf[1] == 'A' && (buf[2] == ' ' || buf[2] == '	'))
+		if (!set_path_to_struct(comp->ea, buf + 2))
+			return(false);
+	if (buf[0] == 'F' && (buf[2] == ' ' || buf[2] == '	'))
+		if (!set_rgb_to_struct(comp->f, buf + 2))
+			return(false);
+	return (true);
+}
+
+bool	parse_input(int fd, t_compass *comp)
 {
 	char	buf[INT_MAX];
 	int		i;
 	ssize_t	r;
 
-	i = 0;
+	i = -1;
 	r = read(fd, buf, INT_MAX);
 	if (!r || r == -1)
 		return (false);
 	while (i < r)
 	{
-		if (check_compass(i, r, buf) == -1)
-			return (false);
+		if (buf[i] != ' ' && buf[i] != '	')
+		{
+			if (!fill_structure_bit(buf + i, comp))
+				return(false);
+			//setup into structure
+			//if structure bit already in use -> return(false)
+		}
 	}
+	return(true);
 }
 
 int	main(int ac, char **av)
 {
 	int	fd;
+	t_compass comp;
 
+	init_compass(&comp);
 	fd = open(av[1], O_RDONLY);
 	if (ac != 2 || !valid_file(av[1]) || fd == -1)
 	{
@@ -66,7 +117,7 @@ int	main(int ac, char **av)
 			return (-1);
 		return (close(fd), -1);
 	}
-	if (!parse_input(fd))
+	if (!parse_input(fd, &comp))
 		return (wrong_format(), close(fd), -1);
 	close(fd);
 }
