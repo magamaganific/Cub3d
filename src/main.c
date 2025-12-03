18695db1fa01715dba15b47eebeb367cb4fbab7d
Maria Fornovi Gomez <mfornovi@c3r6s4.42madrid.com>
2025-12-03 13:05:01 +0100

    parsinggg...

--- src/main.c
+++ src/main.c
@@ -1,27 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfornovi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 11:41:57 by mfornovi          #+#    #+#             */
+/*   Updated: 2025/12/03 11:42:00 by mfornovi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../lib/MLX42/include/MLX42/MLX42.h"
 #include "../include/cube3d.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <limits.h>
 #define WIDTH 900
 #define HEIGHT 900
 
-void print_accepted_input(void)
+void	print_accepted_input(void)
 {
 	printf("Accepted input: \n	./Game [MAP PATH]\n");
-	printf("		(maps available in the \"maps\" directory2)\n");
+	printf("		(maps available in the \"maps\" directory)\n");
 }
 
-int main(int ac, char **av)
+bool	valid_file(char *file_name)
 {
-	int fd;
+	int	i;
+
+	i = -1;
+	while (file_name[++i]);
+	if (file_name[i - 4] != '.' || file_name[i - 3] != 'c'
+			|| file_name[i - 2] != 'u' || file_name[i - 1] != 'b')
+			return (false);
+	return (true);
+}
+
+bool	parse_input(int fd)
+{
+	char	buf[INT_MAX];
+	int		i;
+	ssize_t	r;
+
+	i = 0;
+	r = read(fd, buf, INT_MAX);
+	if (!r || r == -1)
+		return (false);
+	while (i < r)
+	{
+		if (check_compass(i, r, buf) == -1)
+			return (false);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int	fd;
 
 	fd = open(av[1], O_RDONLY);
-	if (ac != 2 || fd == -1)
+	if (ac != 2 || !valid_file(av[1]) || fd == -1)
 	{
 		print_accepted_input();
-		return (-1);
+		if (fd == -1)
+			return (-1);
+		return (close(fd), -1);
 	}
-
+	if (!parse_input(fd))
+		return (wrong_format(), close(fd), -1);
 	close(fd);
 }
