6d9d587d7db1546a00e35053ec7eb36376eb272c
Maria Fornovi Gomez <mfornovi@c2r11s3.42madrid.com>
2026-01-07 11:33:00 +0100

    setup a way to draw a map efficiently into an mlx_image :D

--- include/cube3d.h
+++ include/cube3d.h
@@ -26,7 +26,9 @@
 # define WIDTH 900
 # define HEIGHT 900
 # define BUFFER 100000
-# define MINI 30
+# define SQUARE_SIZE 30
+# define PLAYER_SIZE 10
+
 
 typedef struct s_compass
 {
@@ -41,6 +43,7 @@ typedef struct s_compass
 	mlx_image_t		*ea;
 	mlx_image_t		*bg;
 	mlx_image_t		*map;
+	mlx_image_t		*player;
 	char			*no_path;
 	char			*so_path;
 	char			*we_path;
