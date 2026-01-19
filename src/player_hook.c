18456a87580213a9a36a796cfed82337495868f2
Maria Fornovi Gomez <mfornovi@c1r10s4.42madrid.com>
2026-01-19 12:14:13 +0100

    redistribución de funciones en más archivos para organizar y complacer a la norminette

--- src/player_hook.c
+++ src/player_hook.c
@@ -103,12 +103,12 @@ void	player_hook(void *param)
 		move_left(comp);
 	if (mlx_is_key_down(comp->mlx, MLX_KEY_LEFT))
 	{
-		comp->sight->angle -= 3;
+		comp->sight->angle -= 1;
 		draw_raycaster(comp);
 	}
 	if (mlx_is_key_down(comp->mlx, MLX_KEY_RIGHT))
 	{
-		comp->sight->angle += 3;
+		comp->sight->angle += 1;
 		draw_raycaster(comp);
 	}
 }
